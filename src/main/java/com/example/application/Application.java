bd1a211fb5293784700450d990e62d36cad55a4d
taefi <taefi.soroosh@gmail.com>
2023-05-24 00:49:48 +0300

    update theme name

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-@Theme(value = "my-hilla-react-app")
+@Theme(value = "hilla-react-app")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
